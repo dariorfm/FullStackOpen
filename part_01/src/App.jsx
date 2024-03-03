8345d3d8112879c114109db575f5be372f201fc7
dariorfm <dariorfm@gmail.com>
2024-03-04 00:03:29 +0100

    course obj added to App.jsx

--- part_01/src/App.jsx
+++ part_01/src/App.jsx
@@ -4,11 +4,32 @@ import Anecdotes from "../02_anecdotes/Anecdotes"
 
 function App() {
 
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
 
   return (
     <>
       <div>
-        <CourseInfo />
+        <CourseInfo course={course} />
         <div>========== *** ============</div>
         <Unicafé />
         <div>========== *** ============</div>
