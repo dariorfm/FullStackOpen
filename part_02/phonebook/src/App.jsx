03ab274b073254b109f48a6c04ecb6e3f6c37740
dariorfm <dariorfm@gmail.com>
2024-03-04 18:58:20 +0100

    2.9_step_4 searchTerm functionallity added.

--- part_02/phonebook/src/App.jsx
+++ part_02/phonebook/src/App.jsx
@@ -3,12 +3,18 @@ import { useState } from 'react'
 
 const App = () => {
 
-  // Hooks are used to manage state in functional components
+  // Define the initial state of the phonebook  
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
+  
+  // Hooks for the form
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
   
   // Event handlers
   const handleNameChange = (event) => {
@@ -19,6 +25,11 @@ const App = () => {
   const handleNumberChange = (event) => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value.toLowerCase())
   }
 
   // Check if the person already exists
@@ -42,11 +53,17 @@ const App = () => {
     setNewNumber('')
   }
 
+  // Filter the list of persons based on the search term
+  const filteredPersons = persons.filter(person => person.name.toLowerCase().includes(searchTerm))
+
 
   // Display the phonebook
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input type="text" value={searchTerm} onChange={handleSearchChange} placeholder="Search by name" />
+      </div>
       <form onSubmit={addPerson} >
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -59,8 +76,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>{newName} {newNumber}</div>
 
+      <ul>
+        {filteredPersons.map((person, index) => (
+          <li key={index}>{person.name} {person.number}</li>
+        ))}
+      </ul>
     </div>
   )
 }
