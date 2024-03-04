478542b90851290fa1310d59281c3c2f1fe1c659
dariorfm <dariorfm@gmail.com>
2024-03-04 17:39:10 +0100

    2.8_step_3

--- part_02/phonebook/src/App.jsx
+++ part_02/phonebook/src/App.jsx
@@ -2,35 +2,48 @@
 import { useState } from 'react'
 
 const App = () => {
+
+  // Hooks are used to manage state in functional components
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456' },
   ]) 
   const [newName, setNewName] = useState('')
-
+  const [newNumber, setNewNumber] = useState('')
+  
+  // Event handlers
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
   
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  // Check if the person already exists
   const exists = persons.find(person => person.name === newName)
   
+  // Add a new person to the phonebook
   const addPerson = (event) => {
+
     event.preventDefault()
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
     if (exists) {
       alert(`${newName} is already added to phonebook`)
       return
     }
     setPersons(persons.concat(personObject))
+    
     setNewName('')
+    setNewNumber('')
   }
 
 
-
-
-
+  // Display the phonebook
   return (
     <div>
       <h2>Phonebook</h2>
@@ -39,11 +52,15 @@ const App = () => {
           name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <div>debug: {newName}</div>
+      <div>{newName} {newNumber}</div>
+
     </div>
   )
 }
