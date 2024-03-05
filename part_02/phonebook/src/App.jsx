6db032520db55f5ab82eb9b227b69fb134f1d431
dariorfm <dariorfm@gmail.com>
2024-03-05 02:56:56 +0100

    2.11_step_6

--- part_02/phonebook/src/App.jsx
+++ part_02/phonebook/src/App.jsx
@@ -1,19 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from "./components/Filter"
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 
 const App = () => {
-
+  
   // Define the initial state of the phonebook  
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
-    
+  const [persons, setPersons] = useState([]) 
+  
+  // Fetch the initial state of the phonebook from the server
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
 
   // Display the phonebook
   return (
