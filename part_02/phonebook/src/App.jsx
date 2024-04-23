5531fd57ae1a970ebbacb91117d306f44c9983e8
dariorfm <dariorfm@gmail.com>
2024-04-23 11:13:24 +0200

    Formating App file

--- part_02/phonebook/src/App.jsx
+++ part_02/phonebook/src/App.jsx
@@ -29,100 +29,103 @@ const App = () => {
 
 
 
-// Event handlers
-const handleNameChange = (event) => {
-  setNewName(event.target.value)
-}
-
-const handleNumberChange = (event) => {
-  setNewNumber(event.target.value)
-}
-
-const handleSearchChange = (event) => {
-  console.log(event.target.value)
-  setSearchTerm(event.target.value.toLowerCase())
-}
-
-// Remove a person from the phonebook
-
-const removePerson = (person, id) => {
-  if (window.confirm(`Delete ${person.name}`)) {
-      personService
-      .remove(person.id)
-      .then(() => {
-          setPersons(persons.filter(person => person.id !== id))
-      })
+  // Event handlers
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
   }
-}
 
-
-// Add a new or update number 
-const addPerson = (event) => {
-
-  // Check if the person already exists
-  const exists = persons.find(person => 
-    person.name.toLowerCase() === newName.toLowerCase()
-  )
-
-  event.preventDefault()
-  const personObject = {
-      name: newName,
-      number: newNumber
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
-  if (exists) {
-      if (window.confirm(`${newName} 
-      is already added to phonebook, 
-      replace the old number with a new one?`)) 
-      {
-          const updatedPerson = { ...exists, number: newNumber }   
-          personService
-          .update(updatedPerson.id, updatedPerson)
-          .then(returnedPerson => {
-              setPersons(
-                  persons.map(person => 
-                  person.id !== exists.id 
-                  ? person 
-                  : returnedPerson
-              ))
-              setNewName('')
-              setNewNumber('')
 
-          }).catch(error => {
-              setErrorMessage(
-                  `Information of ${newName} has already been removed from server`
-              )
-              setTimeout(() => {
-                  setErrorMessage(null)
-              }, 5000)
-              setPersons(persons.filter(person => person.id !== exists.id))
-          })
-
-          console.log('Edit confirmed')
-          return
-      } else {
-
-          console.log('Edit cancelled') 
-          return
-      }
-     
+  const handleSearchChange = (event) => {
+    console.log(event.target.value)
+    setSearchTerm(event.target.value.toLowerCase())
   }
-  personService
-  .create(personObject)
-  .then(returnedPerson => {
-      setPersons(persons.concat(returnedPerson))
-      setNewName('')
-      setNewNumber('')
-  })
 
-  console.log('Add confirmed')
-  setConfMessage(
-    `Added ${newName}`
+  // Remove a person from the phonebook
+
+  const removePerson = (person, id) => {
+    if (window.confirm(`Delete ${person.name}`)) {
+        personService
+        .remove(person.id)
+        .then(() => {
+            setPersons(persons.filter(person => person.id !== id))
+        })
+    }
+  }
+
+
+  // Add a new or update number 
+  const addPerson = (event) => {
+
+    // Check if the person already exists
+    const exists = persons.find(person => 
+      person.name.toLowerCase() === newName.toLowerCase()
     )
-    setTimeout(() => {
-      setConfMessage(null)
-    }, 5000
-    )
-}
+
+    event.preventDefault()
+
+    const personObject = {
+        name: newName,
+        number: newNumber
+    }
+    
+    if (exists) {
+        if (window.confirm(`${newName} 
+        is already added to phonebook, 
+        replace the old number with a new one?`)) 
+        {
+            const updatedPerson = { ...exists, number: newNumber }   
+            personService
+            .update(updatedPerson.id, updatedPerson)
+            .then(returnedPerson => {
+                setPersons(
+                    persons.map(person => 
+                    person.id !== exists.id 
+                    ? person 
+                    : returnedPerson
+                ))
+                setNewName('')
+                setNewNumber('')
+
+            }).catch(error => {
+                setErrorMessage(
+                    `Information of ${newName} has already been removed from server`
+                )
+                setTimeout(() => {
+                    setErrorMessage(null)
+                }, 5000)
+                setPersons(persons.filter(person => person.id !== exists.id))
+            })
+
+            console.log('Edit confirmed')
+            return
+        } else {
+
+            console.log('Edit cancelled') 
+            return
+        }
+      
+    }
+
+    personService
+    .create(personObject)
+    .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('')
+        setNewNumber('')
+    })
+
+    console.log('Add confirmed')
+    setConfMessage(
+      `Added ${newName}`
+      )
+      setTimeout(() => {
+        setConfMessage(null)
+      }, 5000
+      )
+  }
 
 
   
